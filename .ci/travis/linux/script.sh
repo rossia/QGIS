7b6d9e8e9e8035e918e413c31c62cb9b9392da91
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-08-15 08:45:13 +0200

    Revert "use second Dockerfile to push 2nd image"
    This reverts commit 050682a37364e110f350b9c3293f662b0011036d.

--- .ci/travis/linux/script.sh
+++ .ci/travis/linux/script.sh
@@ -21,8 +21,6 @@ pushd $DIR
 
 mkdir -p $CCACHE_DIR
 
-docker-compose --version
-docker-compose -f $DOCKER_COMPOSE config
-docker-compose -f $DOCKER_COMPOSE run --rm qgis-build-and-test
+docker-compose -f $DOCKER_COMPOSE run --rm qgis-deps
 
 popd
